52226236cbb43718ebbb15c68906197c37ce38bc
Jonathan Pyrak <bobcatpyrak@gmail.com>
2020-11-10 10:40:30 -0500

    added components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MyCV';
+  title: string = 'MyCV';
+  name: string = "Jonathan J. Pyrak";
+  address: string = "7921 Black Hawk Court";
+  cityState: string = "West Chester, OH 45069";
+  phone: string = "513-289-5619";
+  email: string = "jonathanpyrak@gmail.com";
+  displayEducation: boolean = true;
+
+  favColors: string[] = [
+    "teal",
+    "violet",
+    "silver"
+  ];
 }
